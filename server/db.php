5a21fc79babee95e55c55f65f1b17646d75915f8
iNewLegend <czf.leo123@gmail.com>
2017-06-12 22:39:23 +0300

    db.php updated

--- server/db.php
+++ server/db.php
@@ -1,18 +1,46 @@
 <?php
 /**
-* file      : # db.php
-* author    : czf.leo123@gmail.com
-* todo      : make it better
-* desc      : used to create the DB
-*/
+ * @file    : db.php
+ * @author  : Leonid Vinikov (czf.leo123@gmail.com)
+ * @todo    : Make it better
+ * @desc    : used to create the database
+ */
+
+if(PHP_SAPI !== 'cli' || isset($_SERVER['HTTP_USER_AGENT'])) {
+    exit('Script execute only from CLI');
+}
 
 require 'init.php';
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+/**
+ * DBUilder class
+ */
 class DBUilder
 {
+    /**
+     * Logger instance
+     *
+     * @var \Core\Logger
+     */
+    public $logger;
+    
+    /**
+     * DBUilder constructor
+     */
+    function __construct() 
+    {
+        $this->logger = new \Core\Logger(get_class($this));
+        $this->logger->info("Logger initalized");
+    }
+
+    /**
+     * Used to create users table
+     *
+     * @return void
+     */
     public function users()
     {        
         # Droping Schema
@@ -20,7 +48,8 @@ class DBUilder
         Capsule::schema()->dropIfExists('users');
         Capsule::statement('SET FOREIGN_KEY_CHECKS = 1');
 
-        echo 'drop users table' . '<br />';
+        $this->logger->warning('Drop if Exists `users` table');
+        
         # Creating schema
         Capsule::schema()->create('users', function (Blueprint $table) {
             $table->increments('id');
@@ -29,14 +58,22 @@ class DBUilder
             $table->string('password');
             $table->timestamps();
         });
-        echo 'create users table' . '<br />';
+
+        $this->logger->info('Creating `users` table');
+        $this->logger->debug(json_encode(Capsule::select('DESCRIBE users')));
+        $this->logger->notice('`users` table created');
     }
 
+    /**
+     * Used to create sessions table
+     *
+     * @return void
+     */
     public function sessions()
     {
         # Droping Schema
         Capsule::schema()->dropIfExists('sessions');
-        echo 'drop sessions table' . '<br />';
+        $this->logger->warning('Drop if Exists `sessions` table');
 
         # Creating schema
         Capsule::schema()->create('sessions', function (Blueprint $table) {
@@ -50,14 +87,21 @@ class DBUilder
             $table->string('cookie_crc', 40);
         });
 
-        echo 'create sessions table' . '<br />';
+        $this->logger->info('Creating `sessions` table');
+        $this->logger->debug(json_encode(Capsule::select('DESCRIBE sessions')));
+        $this->logger->notice('`sessions` table created');
     }
 
+    /**
+     * Used to create attempts table
+     *
+     * @return void
+     */
     public function attempts()
     {
         # Droping Schema
         Capsule::schema()->dropIfExists('attempts');
-        echo 'drop attempts table' . '<br />';
+        $this->logger->warning('Drop if Exists `attempts` table');
 
         # Creating schema
         Capsule::schema()->create('attempts', function (Blueprint $table) {
@@ -65,14 +109,22 @@ class DBUilder
             $table->string('ip', 39);
             $table->datetime('expiredate');
         });
-        echo 'create attempts table' . '<br />';
+
+        $this->logger->info('Creating `attempts` table');
+        $this->logger->debug(json_encode(Capsule::select('DESCRIBE attempts')));
+        $this->logger->notice('`attempts` table created');
     }
 
+    /**
+     * Used to create config table
+     *
+     * @return void
+     */
     public function config()
     {
         # Droping Schema
         Capsule::schema()->dropIfExists('config');
-        echo 'drop cofnig table' . '<br />';
+        $this->logger->warning('Drop if Exists `config` table');
 
         # Creating schema
        Capsule::schema()->create('config', function (Blueprint $table) {
@@ -80,103 +132,56 @@ class DBUilder
             $table->string('value', 100)->nullable();
        });
 
-        echo 'create cofnig table' . '<br />';
+        $this->logger->info('Creating `config` table');
+        $this->logger->debug(json_encode(Capsule::select('DESCRIBE config')));
+        $this->logger->notice('`config` table created');
 
-        $config = Controller::Model('Config');
+        $this->logger->info('Creating `rows` table');
 
-        $config = new Models\Config();
-        $config->setting    = 'attack_mitigation_time';
-        $config->value      = '+30 minutes';
-        $config->save();
+        $configs = [
+            ['attack_mitigation_time' => '+30 minutes'],
+            ['attempts_before_verify' => '3'],
+            ['attempts_before_ban' => '5'],
+            ['bcrypt_cost' => '10'],
+            ['cookie_forget' => '+30 minutes'],
+            ['cookie_http' => '0'],
+            ['cookie_name' => 'authID'],
+            ['cookie_path' => ''],
+            ['cookie_remember' => '1 month'],
+            ['cookie_secure' => '0'],
+            ['password_min_score' => '1'],
+            ['verify_email_max_length' => '100'],
+            ['verify_email_min_length' => '5'],
+            ['verify_password_min_length' => '6'],
+            ['captcha_secret_key' => CAPTCHA_SECRET_KEY],
+            ['captcha_site_key' => CAPTCHA_SITE_KEY]
+        ];
 
-        $config = new Models\Config();
-        $config->setting    = 'attempts_before_verify';
-        $config->value      = '3';
-        $config->save();
+        
 
-        $config = new Models\Config();
-        $config->setting    = 'attempts_before_ban';
-        $config->value      = '5';
-        $config->save();
+        foreach($configs as $config) {
+            $model = new Models\Config();
+            $model->setting = key($config);
+            $model->value = $config[key($config)];
+            $model->save();
 
-        $config = new Models\Config();
-        $config->setting    = 'bcrypt_cost';
-        $config->value      = '10';
-        $config->save();
+            $this->logger->debug(key($config) . ' = ' . $config[key($config)]);
 
-        $config = new Models\Config();
-        $config->setting    = 'cookie_forget';
-        $config->value      = '+30 minutes';
-        $config->save();
+        }
 
-        $config = new Models\Config();
-        $config->setting    = 'cookie_http';
-        $config->value      = '0';
-        $config->save();
-
-        $config = new Models\Config();
-        $config->setting    = 'cookie_name';
-        $config->value      = 'authID';
-        $config->save();
-
-        $config = new Models\Config();
-        $config->setting    = 'cookie_path';
-        $config->value      = '';
-        $config->save();
-
-        $config = new Models\Config();
-        $config->setting    = 'cookie_remember';
-        $config->value      = '+1 month';
-        $config->save();
-
-        $config = new Models\Config();
-        $config->setting    = 'cookie_secure';
-        $config->value      = '0';
-        $config->save();
-
-        $config = new Models\Config();
-        $config->setting    = 'password_min_score';
-        $config->value      = '1';
-        $config->save();
-
-        $config = new Models\Config();
-        $config->setting    = 'verify_email_max_length';
-        $config->value      = '100';
-        $config->save();
-
-        $config = new Models\Config();
-        $config->setting    = 'verify_email_min_length';
-        $config->value      = '5';
-        $config->save();
-
-        $config = new Models\Config();
-        $config->setting    = 'verify_password_min_length';
-        $config->value      = '6';
-        $config->save();
-
-        $config = new Models\Config();
-        $config->setting    = 'captcha_secret_key';
-        $config->value      = CAPTCHA_SECRET_KEY;
-        $config->save();
-
-        $config = new Models\Config();
-        $config->setting    = 'captcha_site_key';
-        $config->value      = CAPTCHA_SITE_KEY;
-        $config->save();
-
-        echo 'create cofnig table values' . '<br />';
+        $this->logger->notice('`config` rows created');
     }
 }
+
 
 $db = new DBUilder();
 
 try {
+    
     $db->users();
     $db->sessions();
     $db->attempts();
     $db->config();
-} catch(Exception $e) {
-    echo $e->getMessage();
+} catch(\Exception $e) {
+    $db->logger->error($e->getMessage());
 }
-
-
